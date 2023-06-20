1dfb03491cc7be8481c53bd5995303227f0c3fc1
Avinash <avinash424@gmail.com>
2023-06-20 15:38:27 +0530

    hero

---
+++ Hero/hero.java
@@ -0,0 +1,36 @@
+public class hero{
+public static void main(String args[])
+{
+System.out.println("sum of these numbers:");
+int num1=5;
+int num2=7;
+int num3=9;
+int sum=num1+num2+num3;
+System.out.println("sum=num1+num2+num3");
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
