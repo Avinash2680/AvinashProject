0b4a9cd200f7386f782591aa6e2d782ebb71c923
Avinash <avinash424@gmail.com>
2023-06-20 15:23:46 +0530

    added anil

---
+++ anil/src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        Greet();
+    }
+    static void Greet(){
+        System.out.println("hello");
+    }
+}
---
+++ anil/src/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test1 {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
